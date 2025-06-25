f80ab58631dccbab92affeb426f31400f8b36519
mdshakibsami <mdshakibsami21@gmail.com>
2025-06-26 03:15:01 +0600

    successfully fetch demo data from strapi

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,11 +3,20 @@ import bannerImage from "@/public/cpsCover.jpg";
 import getAllCourses from "./lib/getAllCourses";
 
 export default async function Home() {
+  const coursesObject = await getAllCourses();
+  const courses = coursesObject.data;
+
+  console.log(courses);
   return (
     <main>
       <div style={{ width: "100vw", height: "450px", position: "relative" }}>
         <Image src={bannerImage} alt="cover image" fill />
       </div>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>{course.title}</li>
+        ))}
+      </ul>
     </main>
   );
 }
